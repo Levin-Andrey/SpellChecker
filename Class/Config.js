1e78beb0e229730dfedb6fad42f13a17393a20f1
lenny <lennytmp@gmail.com>
2014-01-07 15:28:09 +0400

    success message fixed

--- Class/Config.js
+++ Class/Config.js
@@ -5,6 +5,6 @@ Config.project = {};
 
 Config.email.from = "noreply@spellchecker.com";
 Config.email.to = "lennytmp@gmail.com";
-Config.project.pages_limit = 10;
+Config.project.pages_limit = 100;
 
 module.exports = Config;
